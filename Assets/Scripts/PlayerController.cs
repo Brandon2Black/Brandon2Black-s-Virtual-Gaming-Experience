30339a5c9f9eb6978918b0a3875546f2f50fc3b1
Brandon2Black <brandonb627@nycstudents.net>
2024-10-15 08:44:30 -0400

    Player Movement Controls Added n stuf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,18 +9,38 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        Vector2 position = transform.position;
-        position.x = position.x - 0.01f;
-        position.y = position.y + 0.01f;
-        transform.position = position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //creating var to do plr movement stuf.
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+        if(Keyboard.current.leftArrowKey.isPressed)
+        {
+          horizontal = -1.0f;
+        }
+     else if(Keyboard.current.rightArrowKey.isPressed)
+        {
+          horizontal = 1.0f;
+        }
+         if(Keyboard.current.upArrowKey.isPressed)
+        {
+          vertical = 1.0f;
+        }
+     else if(Keyboard.current.downArrowKey.isPressed)
+        {
+          vertical = -1.0f;
+        }
+         Debug.Log(horizontal);
+         Debug.Log(vertical);
+        //handling said plr movement stuf.
         Vector2 position = transform.position;
-        position.x = position.x - 0.01f;
-        position.y = position.y + 0.01f;
+        position.x = position.x + 0.05f * horizontal;
+        position.y = position.y + 0.05f * vertical;
         transform.position = position;
+        
     }
 }
