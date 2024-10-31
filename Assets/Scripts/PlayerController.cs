a7017e9a1aca276b87fb4ac5fd4916666d239ed2
Brandon2Black <brandonb627@nycstudents.net>
2024-10-31 09:10:59 -0400

    Collisions!1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction leftAction;
     public InputAction moveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     // Start is called before the first frame update
 
     void Start()
     {
      leftAction.Enable();
      moveAction.Enable();
+     rigidbody2d = GetComponent<Rigidbody2D>();
 
      //QualitySettings.vSyncCount = 0;
      //Application.targetFrameRate = 120;
@@ -22,13 +25,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
       //Stores vector 2 value for movement.
-        Vector2 move = moveAction.ReadValue<Vector2>();
+       move = moveAction.ReadValue<Vector2>();
         //display in console
         Debug.Log(move);
         //handling said plr movement stuf.
-        Vector2 position = (Vector2)transform.position + move * 7.0f * Time.deltaTime;
-        transform.position = position;
+        //Vector2 position = (Vector2)transform.position + move * 7.0f * Time.deltaTime;
+        //transform.position = position;
         //position.x = position.x + 0.04f * horizontal;
         //position.y = position.y + 0.04f * vertical;
     }
+
+    void FixedUpdate()
+   {
+       Vector2 position = (Vector2)rigidbody2d.position + move * 7.0f * Time.deltaTime;
+       rigidbody2d.MovePosition(position);
+   }
 }
