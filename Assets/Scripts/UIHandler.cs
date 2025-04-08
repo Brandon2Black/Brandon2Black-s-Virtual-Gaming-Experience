35d1755e26fd84b3e156dead0dd1de6da9865d11
Brandon2Black <brandonb627@nycstudents.net>
2025-04-08 09:09:18 -0400

    particles donzo

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -24,7 +24,6 @@ public class UIHandler : MonoBehaviour
    // Start is called before the first frame update
    private void Start()
    {
-    
 
        UIDocument uiDocument = GetComponent<UIDocument>();
 
